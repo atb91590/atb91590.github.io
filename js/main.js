b2d1168fb52a6a55b7f510ce375493b8c0c88928
Alex Bu <alex.bu@lacity.org>
2015-04-20 09:47:42 -0700

    Good scroll speed

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@
 	        }
 	        var speed = Math.round(distance / 100);
 	        if (speed >= 20) speed = 20;
-	        var step = Math.round(distance / 100);
+	        var step = Math.round(distance / 50);
 	        var leapY = stopY > startY ? startY + step : startY - step;
 	        var timer = 0;
 	        if (stopY > startY) {
